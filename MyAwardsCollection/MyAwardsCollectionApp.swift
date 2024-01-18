2198add8a4622dd545de7918ea82196c068d10f5
Denis Gindulin <denisgindulin@me.com>
2024-01-18 17:49:05 +0300

    Initial Commit

---
+++ MyAwardsCollection/MyAwardsCollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAwardsCollectionApp.swift
+//  MyAwardsCollection
+//
+//  Created by Денис Гиндулин on 18.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct MyAwardsCollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
